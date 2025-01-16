c151169146b61f769906b7bc6b87a0df15f430e8
Iskam31 <iskam2004qwe@gmail.com>
2025-01-16 18:50:11 +0300

    изменение шорт-кода формы, для статей

--- my_theme/single.php
+++ my_theme/single.php
@@ -22,7 +22,7 @@
     <!-- Форма обратной связи -->
     <section class="contact-form hidden">
         <h2>Свяжитесь с нами</h2>
-        <?php echo do_shortcode('[contact-form-7 id="e7643b6" title="Обратная связь статей"]'); ?>
+        <?php echo do_shortcode('[contact-form-7 id="3f647b6" title="Обратная связь статей"]'); ?>
     </section>
 </main>
 <?php get_footer(); ?>
