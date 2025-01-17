4fff82097b69ae33534b56aead6010a446336c48
Iskam31 <iskam2004qwe@gmail.com>
2025-01-17 17:15:12 +0300

    Решение ошибки пересборки контейнера

--- init.sh
+++ init.sh
@@ -3,3 +3,10 @@ mkdir -p /var/www/html/wp-content/uploads
 mkdir -p /var/www/html/wp-content/upgrade
 chown -R www-data:www-data /var/www/html/wp-content
 exec "$@"
+
+cd /var/www/html/wp-content/themes
+docker-compose down
+docker-compose build --no-cache
+echo "Пересборка контейнеров"
+docker-compose up -d
+echo "Пересборка контейнеров успешна"
